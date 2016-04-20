6df012adacd5375e95b11d790356dc3208d432e1
halsey <wsh940808@163.com>
2016-04-20 17:37:47 +0800

    添加了具体的修改用户界面信息+1

--- app/src/main/java/com/ylsna/activity/EditUserInfoActivity.java
+++ app/src/main/java/com/ylsna/activity/EditUserInfoActivity.java
@@ -2,10 +2,16 @@ package com.ylsna.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.RelativeLayout;
 
 import com.ylsna.R;
 
 public class EditUserInfoActivity extends AppCompatActivity {
+    /**
+     * 修改头像
+     */
+    private RelativeLayout rl_edit_user_info_head;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
