3fb10cf696fa657dd130e6dfb5383c0bdffe7852
halsey <wsh940808@163.com>
2016-04-22 11:39:59 +0800

    增加了买种子界面+1和返回事件

--- app/src/main/java/com/ylsna/activity/IncomeRankListActivity.java
+++ app/src/main/java/com/ylsna/activity/IncomeRankListActivity.java
@@ -2,10 +2,16 @@ package com.ylsna.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
 
 import com.ylsna.R;
 
 public class IncomeRankListActivity extends AppCompatActivity {
+    /**
+     * 返回按钮
+     */
+    private ImageButton imb_income_rank_list_toolbar_back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +21,27 @@ public class IncomeRankListActivity extends AppCompatActivity {
             //隐藏ActionBar
             getSupportActionBar().hide();
         }
+
+        initView();
+        setListener();
+    }
+
+    /**
+     * 初始化控件
+     */
+    private void initView() {
+        imb_income_rank_list_toolbar_back = (ImageButton) findViewById(R.id.imb_income_rank_list_toolbar_back);
+    }
+
+    /**
+     * 设置监听器
+     */
+    private void setListener() {
+        imb_income_rank_list_toolbar_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
